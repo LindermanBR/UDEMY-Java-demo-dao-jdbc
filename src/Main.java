49d5a0e381cac38aff92d673e10cb0a584bc0c15
Linderman Moura <linderman.moura@outlook.com>
2024-01-07 13:36:28 -0300

    Seller entity class

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
-import java.sql.Connection;
 
-import db.DB;
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Main {
     public static void main(String[] args) {
 
         Department department = new Department(1, "Books");
         System.out.println(department);
+        System.out.println("--------------------------------------------------");
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.0, department);
+
+        System.out.println(seller);
     }
 }
