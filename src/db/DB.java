a678c124483cfffd206e5a471f21930315a4ad1d
Linderman Moura <linderman.moura@outlook.com>
2024-01-06 21:43:43 -0300

    Department entity class

--- src/db/DB.java
+++ src/db/DB.java
@@ -3,17 +3,22 @@ package db;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DB {
     private static Connection conn = null;
 
-    public static Connection getConnection(){
-        if(conn == null){
+    public static Connection getConnection() {
+        if (conn == null) {
             try {
                 Properties props = loadProperties();
                 String url = props.getProperty("dburl");
                 conn = DriverManager.getConnection(url, props);
-            }catch (SQLException e){
+            } catch (SQLException e) {
                 throw new DbException(e.getMessage());
             }
 
@@ -21,28 +26,28 @@ public class DB {
         return conn;
     }
 
-    private static Properties loadProperties(){
-        try (FileInputStream fs = new FileInputStream("db.properties")){
+    private static Properties loadProperties() {
+        try (FileInputStream fs = new FileInputStream("db.properties")) {
             Properties props = new Properties();
             props.load(fs);
             return props;
-        }catch (IOException e) {
+        } catch (IOException e) {
             throw new DbException(e.getMessage());
         }
     }
 
-    public static void closeConnection(){
-        try{
-            if(conn != null){
+    public static void closeConnection() {
+        try {
+            if (conn != null) {
                 conn.close();
             }
-        } catch (SQLException e){
+        } catch (SQLException e) {
             throw new DbException(e.getMessage());
         }
     }
 
-    public static void closeStatement(Statement statement){
-        if(statement != null){
+    public static void closeStatement(Statement statement) {
+        if (statement != null) {
             try {
                 statement.close();
             } catch (SQLException e) {
@@ -50,6 +55,7 @@ public class DB {
             }
         }
     }
+
     public static void closeResultSet(ResultSet resultSet) {
         if (resultSet != null) {
             try {
