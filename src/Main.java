2033b22170b40708a880ebed91c108341369b642
Linderman Moura <linderman.moura@outlook.com>
2024-01-07 14:47:19 -0300

    SellerDaoJDBC and DaoFactory

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 
 import java.time.LocalDate;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,7 +13,10 @@ public class Main {
         System.out.println(department);
         System.out.println("--------------------------------------------------");
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.0, department);
-
         System.out.println(seller);
+        System.out.println("--------------------------------------------------");
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
     }
 }
