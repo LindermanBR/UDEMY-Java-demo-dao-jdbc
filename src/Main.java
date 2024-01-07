8c2d42dd100f7e7856a0241d190cf39a377eeb7a
Linderman Moura <linderman.moura@outlook.com>
2024-01-07 17:34:06 -0300

    findById implementation

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,16 @@
 
-import java.time.LocalDate;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
+
 import model.entities.Seller;
 
 public class Main {
     public static void main(String[] args) {
 
-        Department department = new Department(1, "Books");
-        System.out.println(department);
-        System.out.println("--------------------------------------------------");
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.0, department);
-        System.out.println(seller);
-        System.out.println("--------------------------------------------------");
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
 
     }
 }
